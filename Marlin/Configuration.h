83b93d2b8da710e8a843d8486172c290aeea9f17
Eugene Butan <eugene.butan@gmail.com>
2020-05-17 19:02:27 +0100

    Adjusted steps for the current stepper I_RMS

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -740,7 +740,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT    { 80, 80, 400, 421 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT    { 80.6, 80.2, 404, 421 }
 
 /**
  * Default Max Feed Rate (mm/s)
